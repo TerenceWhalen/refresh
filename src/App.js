f164c0c4000c3a9cdacb274d551669506b664280
twhalen <twhalenjr@me.com>
2023-08-05 07:46:04 -0400

    #34

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ToDo from "./components/ToDo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>My ToDo's</h1>
+   <ToDo text='Learn React' />
+   <ToDo text='Master React'/>
+   <ToDo text='Destroy React'/>
+   
+  </div>
   );
 }
+  
+
 
 export default App;
